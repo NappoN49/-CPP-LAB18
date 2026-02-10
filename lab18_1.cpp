85c5617b7162c61ca66f644b224e73fd54745dba
NappoN49 <parawin39228@gmail.com>
2026-02-10 15:47:23 +0700

    lab18_1_2 complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,13 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1,Rect R2){
+    double xs = max(R1.x,R2.x);
+    double ys = min(R1.y,R2.y);
+    double xe = min(R1.x+R1.w,R2.x+R2.w);
+    double ye = max(R1.y-R1.h,R2.y-R2.h);
+    double neww = xe-xs,newh = ys-ye;
+    double area = 0;
+    if(neww>0 && newh>0) area = neww*newh;
+    return area;
 }
